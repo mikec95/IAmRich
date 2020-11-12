bbb4b9f8b2260318438dc503b87601958d3eaca9
Mike Carbonari <mikecarbonari1@gmail.com>
2020-11-12 14:27:36 -0600

    Create initial app with images and icons

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Michael Carbonari on 11/12/20.
+//  Copyright © 2020 Michael Carbonari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
